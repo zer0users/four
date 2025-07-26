160fdd58064d1c2ab30aabd7adba00fda63d5cb8
zer0users <zerousers789@gmail.com>
2025-07-25 21:07:50 -0400

    Create install.py

---
+++ install.py
@@ -0,0 +1,5 @@
+echo "Installing four.."
+sudo -v
+wget https://raw.githubusercontent.com/zer0users/four/refs/heads/main/install-files/four
+sudo mv four /usr/bin/four
+echo "Four installation Done! You can run 'four' on the terminal."
