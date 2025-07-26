c118464f71857242b82b1f3b544ff56f424341b4
zer0users <zerousers789@gmail.com>
2025-07-25 21:09:52 -0400

    Update and rename install.py to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "Installing four.."
 sudo -v
 wget https://raw.githubusercontent.com/zer0users/four/refs/heads/main/install-files/four
+chmod +x four
 sudo mv four /usr/bin/four
 echo "Four installation Done! You can run 'four' on the terminal."
