7f8e94fcf5749176dbcf655f2f4e72d8156c8030
zer0users <zerousers789@gmail.com>
2025-07-25 21:12:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 echo "Installing four.."
 sudo -v
+echo "- Getting four latest version.."
 wget https://raw.githubusercontent.com/zer0users/four/refs/heads/main/install-files/four
+echo "- Giving permissions.."
 chmod +x four
+echo "Moving into user programs.."
 sudo mv four /usr/bin/four
 echo "Four installation Done! You can run 'four' on the terminal."
